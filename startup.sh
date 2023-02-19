19c789bd246c4a32170adb9792d0090a5b9f1db7
Salman Shafique <salmanmalik820@gmail.com>
2023-02-19 15:01:04 +0500

    Network added to services

--- startup.sh
+++ startup.sh
@@ -3,3 +3,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker network create app-net
